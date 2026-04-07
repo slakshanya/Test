11233f33e3fddbe58a7f7642ba961e45d5e7c2ce
slakshanya <slakshanya@gmail.com>
2026-04-07 08:48:51 -0500

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Lakshanya Selvakumar on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestTests/TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestTests.swift
+//  TestTests
+//
+//  Created by Lakshanya Selvakumar on 2/3/26.
+//
+
+import Testing
+@testable import Test
+
+struct TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
